53c6127ee3a6dbaf52e46282eb81bbaef35a4ac7
Jorge Taylor <jorge@macbook-jt.broadband>
2022-12-01 15:20:36 +0000

    added likelihood and classifier. need to fix f1 and update confusion matrix

--- NB_sentiment_analyser.py
+++ NB_sentiment_analyser.py
@@ -1,72 +1,56 @@
 # -*- coding: utf-8 -*-
-"""
-NB sentiment analyser.
-
-Start code.
-"""
-
 import argparse
-import string
 import pandas as pd
+import string
+import numpy as np
 from nltk.corpus import stopwords
-import nltk
+from nltk import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from collections import Counter
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# IMPORTANT, modify this part with your details
 USER_ID = "acb20jt"  # your unique student ID
 
-
 def parse_args():
-    parser = argparse.ArgumentParser(description="A Naive Bayes Sentiment \
-                    Analyser for the Rotten Tomatoes Movie Reviews dataset")
+    parser = argparse.ArgumentParser(description="A Naive Bayes Sentiment Analyser for the Rotten Tomatoes Movie Reviews dataset")
     parser.add_argument("training")
     parser.add_argument("dev")
     parser.add_argument("test")
     parser.add_argument("-classes", type=int)
-    parser.add_argument('-features', type=str, default="all_words",
-                        choices=["all_words", "features"])
-    parser.add_argument('-output_files',
-                        action=argparse.BooleanOptionalAction, default=False)
-    parser.add_argument('-confusion_matrix',
-                        action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('-features', type=str, default="all_words", choices=["all_words", "features"])
+    parser.add_argument('-output_files', action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument('-confusion_matrix', action=argparse.BooleanOptionalAction, default=False)
     args = parser.parse_args()
     return args
 
-
 def load_data(filepath):
+    print('LOAD DATA')
     df = pd.read_csv(filepath, sep='\t', header=0)
     return df
 
-
-# def stoplister():
-# def lemmatizer():
-
-
 def preprocess_data(df):
+    print('PREPROCESS DATA')
     punct = string.punctuation.replace('!', '')  # keep exclamation marks!
     stoplist = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
 
     df['Phrase'] = df['Phrase'].str.lower()
     df['Phrase'] = df['Phrase'].str.translate(str.maketrans('', '', punct))
     df['Phrase'] = df['Phrase'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stoplist)]))
-    # lemmatisation
-    # w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
-    # lemmatizer = nltk.stem.WordNetLemmatizer()
-    # df['Phrase'] = df['Phrase'].apply([lemmatizer.lemmatize(w) for w in w_tokenizer.tokenize(df['Phrase'])])
-
-    # still some dodgy words getting through!
+    df['Phrase'] = df['Phrase'].apply(lambda x: word_tokenize(x))
+    df['Phrase'] = df['Phrase'].apply(lambda x:[lemmatizer.lemmatize(word) for word in x])
     return df
 
-
 def map_sentiment(df):
+    print('MAP SENTIMENTS')
     df['Sentiment'] = df['Sentiment'].replace(1, 0)
     df['Sentiment'] = df['Sentiment'].replace(2, 1)
     df['Sentiment'] = df['Sentiment'].replace(3, 2)
     df['Sentiment'] = df['Sentiment'].replace(4, 2)
     return df
 
-
 def compute_priors(df, classes):
-    print()
     print("COMPUTE PRIORS")
     if classes == 3:
         pos_count = df.loc[df['Sentiment'] == 2, 'Sentiment'].count()
@@ -74,10 +58,8 @@ def compute_priors(df, classes):
         neg_count = df.loc[df['Sentiment'] == 0, 'Sentiment'].count()
         sum_counts = pos_count + neu_count + neg_count
 
-        pos_prior = pos_count / sum_counts
-        neu_prior = neu_count / sum_counts
-        neg_prior = neg_count / sum_counts
-        return pos_prior, neu_prior, neg_prior
+        priors = {2: pos_count / sum_counts, 1: neu_count / sum_counts, 0: neg_count / sum_counts}
+        return priors
     else:
         vpos_count = df.loc[df['Sentiment'] == 4, 'Sentiment'].count()
         pos_count = df.loc[df['Sentiment'] == 3, 'Sentiment'].count()
@@ -86,39 +68,97 @@ def compute_priors(df, classes):
         vneg_count = df.loc[df['Sentiment'] == 0, 'Sentiment'].count()
         sum_counts = vpos_count + pos_count + neu_count + neg_count + vneg_count
 
-        vpos_prior = vpos_count / sum_counts
-        pos_prior = pos_count / sum_counts
-        neu_prior = neu_count / sum_counts
-        neg_prior = neg_count / sum_counts
-        vneg_prior = vneg_count / sum_counts
-        return vpos_prior, pos_prior, neu_prior, neg_prior, vneg_prior
-
+        priors = {4: vpos_count / sum_counts, 3: pos_count / sum_counts, 2: neu_count / sum_counts, 1: neg_count / sum_counts, 0: vneg_count / sum_counts}
+        return priors
 
 def compute_likelihoods(df):
-    print()
     print("COMPUTE LIKELIHOODS")
-    two_counts = {}
-    every_word = ' '.join([i for i in df['Phrase']]).split()
 
-    positives = df.loc[df['Sentiment'] == 2]
-    neutrals = df.loc[df['Sentiment'] == 1]
-    negatives = df.loc[df['Sentiment'] == 0]
-    pos_words = ' '.join([i for i in positives['Phrase']]).split()
-    neu_words = ' '.join([i for i in neutrals['Phrase']]).split()
-    neg_words = ' '.join([i for i in negatives['Phrase']]).split()
-    print("positive words", len(pos_words))
-    print("neutral words", len(neu_words))
-    print("negative words", len(neg_words))
+    df_positive = df.loc[df['Sentiment'] == 2]
+    df_neutral = df.loc[df['Sentiment'] == 1]
+    df_negative = df.loc[df['Sentiment'] == 0]
+    vocabulary = df_positive['Phrase'].tolist() + df_neutral['Phrase'].tolist() + df_negative['Phrase'].tolist()
+    print(len(vocabulary))
+    df_list = [(2, df_positive), (1, df_neutral), (0, df_negative)]
 
-    for word in every_word:
-        # print(word)
-        if word in positives:
-            two_counts[word] += 1
-        else:
-            two_counts[word] = 1
-    print(two_counts)
-    return False
+    words = {2: [], 1: [], 0: []}
+    for class_, df in df_list:
+        for row in df['Phrase']:
+            if class_ == 2:
+                words[2].extend(row)
+            elif class_ == 1:
+                words[1].extend(row)
+            else:
+                words[0].extend(row)
 
+    wordlist = []
+    for row in df['Phrase']:
+        wordlist.extend(row)
+
+    likelihoods = {}
+    likelihoods[2] = {k: (v + 1) / (len(words[2]) + len(vocabulary)) for k, v in dict(Counter(words[2])).items()}
+    likelihoods[1] = {k: (v + 1) / (len(words[1]) + len(vocabulary)) for k, v in dict(Counter(words[1])).items()}
+    likelihoods[0] = {k: (v + 1) / (len(words[0]) + len(vocabulary)) for k, v in dict(Counter(words[0])).items()}
+
+    for _class in likelihoods:
+        for word in wordlist:
+            if word not in likelihoods[_class]:
+                # likelihoods[_class][word] = 0
+                likelihoods[_class][word] = 1 / (len(words[_class]) + len(vocabulary)) 
+
+    print(likelihoods)
+    return likelihoods
+
+def classifier(df, priors, likelihoods, classes):
+    print("CLASSIFIER")
+
+    # a function which takes a sentence and returns the most likely class
+    # for each word in the sentence, multiply the likelihood of that word given the class by the prior of the class
+    sentiments = []
+    for row in df['Phrase']:
+        print(row)
+        scores = {2: None, 1: None, 0: None}
+        for _class in likelihoods:
+            calc = priors[_class]
+            for word in row:
+                if word in likelihoods[_class]:
+                    calc *= likelihoods[_class][word]
+                    # print(_class, word, calc)
+            scores[_class] = calc
+        # print(scores) 
+        best = max(scores, key=scores.get)
+        sentiments.append(best)
+    df['Predicted'] = sentiments
+
+    print(df.to_string())
+    return df
+
+def macro_f1(df, classes):
+    print("MACRO F1")
+
+    for class_ in range(classes):
+        print(class_, '------------------------------------------')
+        tp = df.loc[(df['Sentiment'] == class_) & (df['Predicted'] == class_), 'Sentiment'].count()
+        tn = df.loc[(df['Sentiment'] == 1) & (df['Predicted'] == 1), 'Sentiment'].count()
+        fp = df.loc[(df['Sentiment'] == 1) & (df['Predicted'] == 2), 'Sentiment'].count()
+        fn = df.loc[(df['Sentiment'] == 2) & (df['Predicted'] == 1), 'Sentiment'].count()
+
+        print(tp, tn, fp, fn)
+        print(sum([tp, tn, fp, fn]))
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        f1 = 2 * ((precision * recall) / (precision + recall))
+        print("F1: ", f1)
+
+def compute_confusion_matrix(df, classes):
+    print("CONFUSION MATRIX")
+
+    confusion_matrix = np.zeros((classes, classes))
+    for i in range(classes):
+        for j in range(classes):
+            confusion_matrix[i][j] = df.loc[(df['Sentiment'] == i) & (df['Predicted'] == j), 'Sentiment'].count()
+    print(confusion_matrix)
+    print(confusion_matrix.sum())
 
 def main():
     inputs = parse_args()
@@ -140,23 +180,18 @@ def main():
     if number_classes == 3:
         df_training = map_sentiment(df_training)
         df_dev = map_sentiment(df_dev)
-        print(df_training.head(5))
-        positive_prior, neutral_prior, negative_prior = compute_priors(df_training, number_classes)
-        print(f'posp: {positive_prior}, neup: {neutral_prior}, negp: {negative_prior}, sum: {positive_prior + neutral_prior + negative_prior}')
+        priors = compute_priors(df_training, number_classes)
     else:
-        print('5 classes!')
-        very_positive_prior, positive_prior, neutral_prior, negative_prior, very_negative_prior = compute_priors(df_training, number_classes)
-        print(f'posp: {positive_prior}, neup: {neutral_prior}, negp: {negative_prior}, sum: {positive_prior + neutral_prior + negative_prior}')
+        priors = compute_priors(df_training, number_classes)
 
-
-    compute_likelihoods(df_training)
-
-
-
-
-
+    likelihoods = compute_likelihoods(df_training)
+    results = classifier(df_dev, priors, likelihoods, number_classes)
+    macro_f1(results, number_classes)
     # You need to change this to return your macro-F1 score for the dev set
     f1_score = 0
+
+    if confusion_matrix:
+        compute_confusion_matrix(results, number_classes)
 
     """
     IMPORTANT: your code should return the lines below.
@@ -165,7 +200,6 @@ def main():
     """
     # print("Student\tNumber of classes\tFeatures\tmacro-F1(dev)\tAccuracy(dev)")
     print("%s\t%d\t%s\t%f" % (USER_ID, number_classes, features, f1_score))
-
 
 if __name__ == "__main__":
     main()
