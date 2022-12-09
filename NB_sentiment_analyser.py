15cb8a65ec75d5cb15abfaa19f7a45a75ad53850
Jorge Taylor <jorge@mac.broadband>
2022-12-09 03:08:14 +0000

    added comments, improved efficiency, refactored, fixed bug in likelihood computation

--- NB_sentiment_analyser.py
+++ NB_sentiment_analyser.py
@@ -10,9 +10,14 @@ from collections import Counter
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-USER_ID = "acb20jt"  # your unique student ID
+USER_ID = "acb20jt"
 
 def parse_args():
+    '''
+    Parse command line arguments, set defaults where no argument is provided.
+    Return:
+        args: the parsed arguments
+    '''
     parser = argparse.ArgumentParser(description="A Naive Bayes Sentiment Analyser for the Rotten Tomatoes Movie Reviews dataset")
     parser.add_argument("training")
     parser.add_argument("dev")
@@ -22,170 +27,211 @@ def parse_args():
     parser.add_argument('-output_files', action=argparse.BooleanOptionalAction, default=False)
     parser.add_argument('-confusion_matrix', action=argparse.BooleanOptionalAction, default=False)
     args = parser.parse_args()
+
     return args
 
 def load_data(filepath):
+    '''
+    Load data from file as a pandas dataframe.
+    Args:
+        filepath: the path to the file
+    Return:
+        df: the loaded dataframe
+    '''
     df = pd.read_csv(filepath, sep='\t', header=0)
+
     return df
 
 def save_data(df, filename):
-    # remove unnecessary columns
+    '''
+    Save data to file as a tab-separated file.
+    Remove unnecessary columns and renames the predicted sentiment column to 'Sentiment'.
+    Args:
+        df: the dataframe to save
+        filename: the name of the file to save to
+    '''
     df.drop(['Phrase'], axis=1, inplace=True)
+
     if 'Sentiment' in df.columns:
         df.drop(['Sentiment'], axis=1, inplace=True)
-    # rename predicted to sentiment
+
     df.rename(columns={'Predicted': 'Sentiment'}, inplace=True)
-    # save to file
     df.to_csv('predictions/'+filename, sep='\t', index=False)
 
 def preprocess_data(df, features):
-    punct = string.punctuation.replace('!', '')  # keep exclamation marks!
+    '''
+    Preprocess the data by removing punctuation and numbers
+    Also lowercase every word and lemmatize.
+    If specific features are specified, remove stopwords.
+    Args:
+        df: the dataframe to preprocess
+        features: the features to use
+    Return:
+        df: the preprocessed dataframe
+    '''
     numbers = string.digits
+    punct = string.punctuation.replace('!', '')  # keep exclamation marks!
     stoplist = set(stopwords.words('english'))
-    # add custom words to stoplist
-    stoplist.update({'nt', 'n', 'lrb', 'nrb', 'rrb', 'le', 'wo', 'pb'})
+    stoplist.update({'nt', 'n', 'lrb', 'nrb', 'rrb', 'le', 'wo', 'pb'})  # add some more stopwords
     lemmatizer = WordNetLemmatizer()
 
     df['Phrase'] = df['Phrase'].str.lower()
-    df['Phrase'] = df['Phrase'].str.translate(str.maketrans('', '', punct))
     df['Phrase'] = df['Phrase'].str.translate(str.maketrans('', '', numbers))
+    df['Phrase'] = df['Phrase'].str.translate(str.maketrans('', '', punct))
     if features == 'features':
         df['Phrase'] = df['Phrase'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stoplist)]))
     df['Phrase'] = df['Phrase'].apply(lambda x: word_tokenize(x))
     df['Phrase'] = df['Phrase'].apply(lambda x:[lemmatizer.lemmatize(word) for word in x])
+
     return df
 
 def map_sentiment(df):
+    '''
+    If 3 classes are specified, map the sentiment values to remove very positive and very negative.
+    Adjust the sentiment values to be 0, 1, 2.
+    Args:
+        df: the dataframe to map
+    Return:
+        df: the mapped dataframe
+    '''
     df['Sentiment'] = df['Sentiment'].replace(1, 0)
     df['Sentiment'] = df['Sentiment'].replace(2, 1)
     df['Sentiment'] = df['Sentiment'].replace(3, 2)
     df['Sentiment'] = df['Sentiment'].replace(4, 2)
+
     return df
 
 def compute_priors(df, classes):
-    if classes == 3:
-        pos_count = df.loc[df['Sentiment'] == 2, 'Sentiment'].count()
-        neu_count = df.loc[df['Sentiment'] == 1, 'Sentiment'].count()
-        neg_count = df.loc[df['Sentiment'] == 0, 'Sentiment'].count()
-        sum_counts = pos_count + neu_count + neg_count
-        priors = {2: pos_count / sum_counts, 1: neu_count / sum_counts, 0: neg_count / sum_counts}
-    else:
-        vpos_count = df.loc[df['Sentiment'] == 4, 'Sentiment'].count()
-        pos_count = df.loc[df['Sentiment'] == 3, 'Sentiment'].count()
-        neu_count = df.loc[df['Sentiment'] == 2, 'Sentiment'].count()
-        neg_count = df.loc[df['Sentiment'] == 1, 'Sentiment'].count()
-        vneg_count = df.loc[df['Sentiment'] == 0, 'Sentiment'].count()
-        sum_counts = vpos_count + pos_count + neu_count + neg_count + vneg_count
-        priors = {4: vpos_count / sum_counts, 3: pos_count / sum_counts, 2: neu_count / sum_counts, 1: neg_count / sum_counts, 0: vneg_count / sum_counts}
+    '''
+    Compute the priors for the specified number of classes.
+    Args:
+        df: the dataframe to compute the priors from
+        classes: the number of classes
+    Return:
+        priors: a dictionary with the priors for each class
+    '''
+    priors = {}
+    sum_counts = df['Sentiment'].count()
+
+    for _class in range(classes):
+        count = df.loc[df['Sentiment'] == _class, 'Sentiment'].count()
+        priors[_class] = count / sum_counts
+
     return priors
 
 def compute_likelihoods(df, classes):
-    if classes == 3:
-        df_positive = df.loc[df['Sentiment'] == 2]
-        df_neutral = df.loc[df['Sentiment'] == 1]
-        df_negative = df.loc[df['Sentiment'] == 0]
-        # print(df_positive['Phrase'].tolist()[:10])
-        vocabulary = df_positive['Phrase'].tolist() + df_neutral['Phrase'].tolist() + df_negative['Phrase'].tolist()
-        df_list = [(2, df_positive), (1, df_neutral), (0, df_negative)]
-        words = {2: [], 1: [], 0: []}
-    else:
-        df_vpositive = df.loc[df['Sentiment'] == 4]
-        df_positive = df.loc[df['Sentiment'] == 3]
-        df_neutral = df.loc[df['Sentiment'] == 2]
-        df_negative = df.loc[df['Sentiment'] == 1]
-        df_vnegative = df.loc[df['Sentiment'] == 0]
-        vocabulary = df_vpositive['Phrase'].tolist() + df_positive['Phrase'].tolist() + df_neutral['Phrase'].tolist() + df_negative['Phrase'].tolist() + df_vnegative['Phrase'].tolist()
-        df_list = [(4, df_vpositive), (3, df_positive), (2, df_neutral), (1, df_negative), (0, df_vnegative)]
-        words = {4: [], 3: [], 2: [], 1: [], 0: []}
-
-    for class_, df in df_list:
-        for row in df['Phrase']:
-            if classes == 3:
-                if class_ == 2:
-                    words[2].extend(row)
-                elif class_ == 1:
-                    words[1].extend(row)
-                else:
-                    words[0].extend(row)
-            else:
-                if class_ == 4:
-                    words[4].extend(row)
-                elif class_ == 3:
-                    words[3].extend(row)
-                elif class_ == 2:
-                    words[2].extend(row)
-                elif class_ == 1:
-                    words[1].extend(row)
-                else:
-                    words[0].extend(row)
-
-    wordlist = []
-    for row in df['Phrase']:
-        wordlist.extend(row)
-
-    vocab = []
+    '''
+    Compute the likelihoods for the specified number of classes.
+    Args:
+        df: the dataframe to compute the likelihoods from
+        classes: the number of classes
+    Return:
+        likelihoods: a dictionary with the likelihoods for each class
+    '''
+    # calculate words where key is class and value is list of words from the reviews of that class
+    words = {key: [] for key in range(classes)}
+    for class_ in range(classes):
+        for row in df.loc[df['Sentiment'] == class_, 'Phrase']:
+            words[class_].extend(row)
+                
+    # calcalate vocabulary which is a list of all unique words from all reviews
+    vocabulary = df['Phrase'].tolist()
+    vocab = set()
     for row in vocabulary:
         for word in row:
-            if word not in vocab:
-                vocab.append(word)
-    print(len(vocab))
+            vocab.add(word)
     
+    # calculate likelihoods for each word in vocabulary for each class
     likelihoods = {}
-    if classes == 5:
-        likelihoods[4] = {k: (v + 1) / (len(words[4]) + len(vocab)) for k, v in dict(Counter(words[4])).items()}
-        likelihoods[3] = {k: (v + 1) / (len(words[3]) + len(vocab)) for k, v in dict(Counter(words[3])).items()}
-    likelihoods[2] = {k: (v + 1) / (len(words[2]) + len(vocab)) for k, v in dict(Counter(words[2])).items()}
-    likelihoods[1] = {k: (v + 1) / (len(words[1]) + len(vocab)) for k, v in dict(Counter(words[1])).items()}
-    likelihoods[0] = {k: (v + 1) / (len(words[0]) + len(vocab)) for k, v in dict(Counter(words[0])).items()}
+    for class_ in range(classes):
+        likelihoods[class_] = {k: (v + 1) / (len(words[class_]) + len(vocab)) for k, v in dict(Counter(words[class_])).items()}
 
+    # add 1 smoothing for words that are not in the vocabulary
     for _class in likelihoods:
-        for word in wordlist:
+        for word in vocab:
             if word not in likelihoods[_class]:
                 likelihoods[_class][word] = 1 / (len(words[_class]) + len(vocab)) 
 
     return likelihoods
 
 def classifier(df, priors, likelihoods, classes):
+    '''
+    Classify the test data using the priors and likelihoods. 
+    Args:
+        df: the dataframe to classify
+        priors: the priors for each class
+        likelihoods: the likelihoods for each class
+        classes: the number of classes
+    Return:
+        df: the classified dataframe
+    '''
     sentiments = []
+
     for row in df['Phrase']:
-        if classes == 3:
-            scores = {2: None, 1: None, 0: None}
-        else:
-            scores = {4: None, 3: None, 2: None, 1: None, 0: None}
+        scores = {key: None for key in range(classes)}
         for _class in likelihoods:
             calc = priors[_class]
             for word in row:
                 if word in likelihoods[_class]:
                     calc *= likelihoods[_class][word]
             scores[_class] = calc
-        best = max(scores, key=scores.get)
-        sentiments.append(best)
+        highest_score = max(scores, key=scores.get)
+        sentiments.append(highest_score)
     df['Predicted'] = sentiments
+
     return df
 
 def macro_f1(df, classes):
+    '''
+    Compute the macro f1 score for the specified number of classes.
+    Args:
+        df: the dataframe to compute the macro f1 score from
+        classes: the number of classes
+    Return:
+        macro_f1: the macro f1 score
+    '''
     macro_f1 = 0
+
     for class_ in range(classes):
         tp = df.loc[(df['Sentiment'] == class_) & (df['Predicted'] == class_), 'Sentiment'].count()
         fp = df.loc[(df['Sentiment'] == class_) & (df['Predicted'] > class_), 'Sentiment'].count()
         fn = df.loc[(df['Sentiment'] == class_) & (df['Predicted'] < class_), 'Sentiment'].count()
-
         precision = tp / (tp + fp)
         recall = tp / (tp + fn)
         f1 = 2 * ((precision * recall) / (precision + recall))
         macro_f1 += f1
     macro_f1 /= classes
+
     return macro_f1
 
 def compute_confusion_matrix(df, classes):
+    '''
+    Compute the confusion matrix for the specified number of classes.
+    Uses the seaborn library to plot the confusion matrix.
+    Args:
+        df: the dataframe to compute the confusion matrix from
+        classes: the number of classes
+    '''
     confusion_matrix = np.zeros((classes, classes))
     for i in range(classes):
         for j in range(classes):
             confusion_matrix[i][j] = df.loc[(df['Sentiment'] == i) & (df['Predicted'] == j), 'Sentiment'].count()
-    sns.heatmap(confusion_matrix, annot=True, fmt='.1f', cmap='Blues')
+    cm = sns.heatmap(confusion_matrix, annot=True, fmt='.1f', cmap='Blues')
+    cm.set(xlabel='Predicted', ylabel='Actual')
     plt.show()
 
 def main():
+    '''
+    Main function to run the program.
+    1. Parses the command line arguments
+    2. Preprocesses the data
+    3. Maps sentiments if necessary
+    4. Computes the priors and likelihoods
+    5. Classifies the data
+    6. Computes the macro f1 score
+    7. Computes the confusion matrix if specified
+    8. Writes the results to the specified output files if specified
+    '''
     inputs = parse_args()
     training = inputs.training
     dev = inputs.dev
@@ -195,12 +241,10 @@ def main():
     output_files = inputs.output_files
     confusion_matrix = inputs.confusion_matrix
 
-    # load and preprocess data
     df_training = preprocess_data(load_data(training), features)
     df_dev = preprocess_data(load_data(dev), features)
     df_test = preprocess_data(load_data(test), features)
     
-    # map sentiment to 3 classes 
     if number_classes == 3:
         df_training = map_sentiment(df_training)
         df_dev = map_sentiment(df_dev)
@@ -211,6 +255,7 @@ def main():
     results_test = classifier(df_test, priors, likelihoods, number_classes)
     f1_score = macro_f1(results_dev, number_classes)
 
+    # note: the confusion matrix must be closed before the output files are saved
     if confusion_matrix:
         compute_confusion_matrix(results_dev, number_classes)
 
